3ded8a397700208afeb6a7ff86207ec47878734c
Todor Dakov <tdd@dekom.bg>
2014-11-27 02:07:01 +0200

    Update

---
+++ C# Part0/Homework/5. Conditional-Statements-Homework/01. Exchange If Greater/ExchangeIfGreater.cs
@@ -0,0 +1,22 @@
+﻿// Problem 1.	Exchange If Greater
+// Write an if-statement that takes two integer variables a and b and exchanges their values if the first one is greater than the second one. As a result print the values a and b, separated by a space. 
+
+using System;
+
+class ExchangeIfGreater
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        if (a > b)
+        {
+            Console.WriteLine("{0} {1}", b, a);
+        }
+        else
+        {
+            Console.WriteLine("{0} {1}", a, b);
+        }
+    }
+}
