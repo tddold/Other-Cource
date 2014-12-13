da73448abb6d199ac5ad888f751cf85eb0b83a3e
Todor Dakov <tdd@dekom.bg>
2014-12-13 02:09:11 +0200

    Update

--- C# Part0/Homework/7. CSharp-Advanced-Topics-Homework/05. Sorting Numbers/SortingNumbers.cs
+++ C# Part0/Homework/7. CSharp-Advanced-Topics-Homework/05. Sorting Numbers/SortingNumbers.cs
@@ -14,29 +14,81 @@ class SortingNumbers
 
         for (int i = 0; i < n; i++)
         {
-            sequenceNumbers[i] = int.Parse(Console.ReadLine()); 
+            sequenceNumbers[i] = int.Parse(Console.ReadLine());
         }
 
 
         // output        
-        Console.WriteLine(string.Join(",", SortingSequenceNumbers(sequenceNumbers)));
+        Console.WriteLine("Sorting by Selection Sort Algorithm: {0}", string.Join(",", SortingSequenceNumbersSelectionSort(sequenceNumbers)));
+        Console.WriteLine(new string('-', 40));
+        Console.WriteLine("Sorting by Bubble Sort Algorithm: {0}", string.Join(",", SortingSequenceNumbersBubbleSort(sequenceNumbers)));
+        Console.WriteLine(new string('-', 40));
     }
 
-    private static int[] SortingSequenceNumbers(int[] sequenceNumbers)
+    private static IEnumerable<int> SortingSequenceNumbersBubbleSort(int[] sequenceNumbers)
     {
-        for (int i = 0; i < sequenceNumbers.Length; i++)
+        List<int> result = new List<int>();
+
+        bool isSorted = false;
+        while (!isSorted)
         {
-            for (int j = i+1; j < sequenceNumbers.Length; j++)
+            isSorted = true;
+            for (int i = 0; i < sequenceNumbers.Length - 1; i++)
             {
-                if (sequenceNumbers[i] > sequenceNumbers[j])
+                if (sequenceNumbers[i] > sequenceNumbers[i + 1])
                 {
-                    int temp = sequenceNumbers[i];
-                    sequenceNumbers[i] = sequenceNumbers[j];
-                    sequenceNumbers[j] = temp;
+                    isSorted = false;
+                    var temp = sequenceNumbers[i];
+                    sequenceNumbers[i] = sequenceNumbers[i + 1];
+                    sequenceNumbers[i + 1] = temp;
                 }
             }
         }
 
-        return sequenceNumbers;
+        for (int i = 0; i < sequenceNumbers.Length; i++)
+        {
+            if (i == 0)
+            {
+                result.Add(sequenceNumbers[i]);
+            }
+            else if (result[result.Count - 1] != sequenceNumbers[i])
+            {
+                result.Add(sequenceNumbers[i]);
+            }
+        }
+
+        return result;
+    }
+
+    private static List<int> SortingSequenceNumbersSelectionSort(int[] sequenceNumbers)
+    {
+        List<int> result = new List<int>();
+
+        for (int i = 0; i < sequenceNumbers.Length; i++)
+        {
+            int min = i;
+            for (int j = i + 1; j < sequenceNumbers.Length; j++)
+            {
+                if (sequenceNumbers[min] > sequenceNumbers[j])
+                {
+                    min = j;
+                }
+            }
+
+            if (i == 0)
+            {
+                result.Add(sequenceNumbers[min]);
+            }
+            else if (result[result.Count - 1] != sequenceNumbers[min])
+            {
+                result.Add(sequenceNumbers[min]);
+            }
+
+            int temp = sequenceNumbers[min];
+            sequenceNumbers[min] = sequenceNumbers[i];
+            sequenceNumbers[i] = temp;
+        }
+
+        return result;
     }
 }
---
+++ C# Part0/Homework/7. CSharp-Advanced-Topics-Homework/06. Longest Area in Array/LongestAreaInArray.cs
@@ -0,0 +1,61 @@
+﻿// Problem 6.	Longest Area in Array
+// Write a program to find the longest area of equal elements in array of strings. You first should read an integer n and n strings (each at a separate line), then find and print the longest sequence of equal elements (first its length, then its elements). If multiple sequences have the same maximal length, print the leftmost of them. 
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LongestAreaInArray
+{
+    static void Main()
+    {
+        // input
+        int n = int.Parse(Console.ReadLine());
+        string[] array = new string[n];
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = Console.ReadLine();
+        }
+
+        // output
+        //Console.WriteLine(string.Join("\n", array));
+        //return;
+        Console.WriteLine(string.Join("\n", CounterLongestArea(count, array)));
+
+    }
+
+    private static List<string> CounterLongestArea(int count, string[] array)
+    {
+        bool isFalse = false;
+        int countMax = 0;
+        List<string> result = new List<string>();
+        result.Add(array[0]);
+
+       
+            for (int i = 1; i < array.Length; i++)
+            {               
+                if (array[i] == array[i - 1])
+                {                    
+                    result.Add(array[i]);
+                    count++;
+                }
+                else
+                {
+                    if (countMax < count)
+                    {
+                        countMax = count;
+                        result.RemoveAt(i);
+                    }
+
+                    count = 0;
+                }
+           }
+        
+
+        return result;
+    }
+}
