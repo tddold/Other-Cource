07ed73c5247ef3976f7dafecc496a3dad66d8fce
Todor Dakov <tdd@dekom.bg>
2014-12-17 02:49:14 +0200

    Update

---
+++ C# Part0/SoftUni Judge/Basics Exam 7 November 2014/01. Budget/Budget.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Budget
+{
+    class Budget
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            long p = long.Parse(Console.ReadLine());
+            long h = long.Parse(Console.ReadLine());
+
+            long normalWeekends= 4- h;
+            long hometownWeekends = h;
+            long normalWeekendsDay = normalWeekends * 2;
+            long weekdaysOut = p;
+            long weekdaysNormal = 22- weekdaysOut;
+            long renta = 150;
+
+            long expense = normalWeekendsDay * 20;
+            expense += weekdaysOut * ((n * 3 / 100) + 10);
+            expense += weekdaysNormal * 10 + renta;
+
+            long total = n - expense;
+
+            if (total == 0)
+            {
+                Console.WriteLine("Exact Budget.");
+            }
+            else if (total > 0 )
+            {
+                Console.WriteLine("Yes, leftover {0}.", total);
+            }
+            else
+            {
+                Console.WriteLine("No, not enough {0}.", Math.Abs(total));
+            }
+        }
+    }
+}
--- C# Part0/SoftUni Judge/Basics Exam 7 November 2014/03.Plane/Plane.cs
+++ C# Part0/SoftUni Judge/Basics Exam 7 November 2014/03.Plane/Plane.cs
@@ -17,21 +17,48 @@ namespace _03.Plane
             Console.WriteLine(dotsOut + "*" + dotsOut);
 
             string dotsIn;
-            for (int i = 0; i < n-n / 3; i++)
+       
+                for (int i = 0; i < n / 2 + 2; i++)
+                {
+                    dotsOut = new string('.', ((3 * n) - 3) / 2 - i);
+                    dotsIn = new string('.', 2 * i + 1);
+                    Console.WriteLine(dotsOut + "*" + dotsIn + "*" + dotsOut);
+                }
+           
+            
+
+            for (int i = 0; i < n / 2 - 1; i++)
             {
-                dotsOut = new string('.', n + n / 3 - i);
-                dotsIn = new string('.', 2 * i + 1);
+                dotsOut = new string('.', n - 4 - (2 * i));
+                dotsIn = new string('.', ((3 * n) - (2 * (n - 3))) + 4 * i);
                 Console.WriteLine(dotsOut + "*" + dotsIn + "*" + dotsOut);
             }
 
-            for (int i = 0; i < n / 3; i++)
+            dotsOut = new string('.', n - 2);
+            dotsIn = new string('.', n);
+
+            Console.WriteLine("*" + dotsOut + "*" + dotsIn + "*" + dotsOut + "*");
+
+            for (int i = 1; i <= n / 2 - 1; i++)
             {
-                dotsOut = new string('.', n / 2 -1  - 2 * i);
-                dotsIn = new string('.', ((3 * n) - (2 * (n-3))) - i);
-                Console.WriteLine(dotsOut + "*" + dotsIn + "*" + dotsOut);
-                
+                dotsOut = new string('.', n - 2 - 2 * i);
+                string dotsBetween = new string('.', 2 * i - 1);
+                dotsIn = new string('.', n);
+                Console.WriteLine("*" + dotsOut + "*" + dotsBetween + "*" + dotsIn + "*" + dotsBetween + "*" + dotsOut + "*");
             }
-            Console.WriteLine(n / 4);
+
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                dotsOut = new string('.', n - 1 - i);
+                dotsIn = new string('.', n + 2 * i);
+                Console.WriteLine(dotsOut + "*" + dotsIn + "*" + dotsOut);
+            }
+
+
+            string asterix = new string('*', 3 * n);
+            Console.WriteLine(asterix);
+
         }
     }
 }
