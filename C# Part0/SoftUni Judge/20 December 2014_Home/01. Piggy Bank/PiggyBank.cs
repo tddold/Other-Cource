779d9a7712d1f07934fdd6c310c617c3b7c4170d
Todor Dakov <tdd@dekom.bg>
2014-12-21 02:01:33 +0200

    Update

---
+++ C# Part0/SoftUni Judge/20 December 2014_Home/01. Piggy Bank/PiggyBank.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Piggy_Bank
+{
+    class PiggyBank
+    {
+        static void Main(string[] args)
+        {
+            double price = double.Parse(Console.ReadLine());
+            int partyDay = int.Parse(Console.ReadLine());
+
+            int saveCash = (30 - partyDay) * 2;
+            int partyCash = partyDay * 5;
+            int average = saveCash - partyCash;
+
+            int counCash = 0;
+            int years = 0;
+            int month = 0;
+            //int countYears = 0;
+            //int countmonth = 0;
+
+            double neededMonth = price / average;
+
+            int twoDigitsAfterDots = (int) ((neededMonth / 12) * 100) % 10;
+            int firstDigitsAfterDots = (int) ((neededMonth / 12) * 10) % 10;
+
+            if (twoDigitsAfterDots > 0 && twoDigitsAfterDots < 5)
+            {
+                firstDigitsAfterDots++;
+                month = firstDigitsAfterDots;
+            }
+            else
+            {                
+                firstDigitsAfterDots = firstDigitsAfterDots++ + 2;
+                month = firstDigitsAfterDots;
+            }
+
+            month = firstDigitsAfterDots;
+            years = (int) neededMonth / 12;
+            if (years < 0 || month < 0)
+            {
+                Console.WriteLine("never");
+            }
+            else
+            {
+                Console.WriteLine("{0} years, {1} months", years, month);
+            }
+           
+        }
+    }
+}
---
+++ C# Part0/SoftUni Judge/20 December 2014_Home/05. Bit Lock/BitLock.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Bit_Lock
+{
+    class BitLock
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split(' ');
+            string input = string.Empty;
+            string testCol = string.Empty;
+            int val = 0;
+            int mask;
+            int bits;
+            
+
+            do
+            {
+                input = Console.ReadLine();
+
+                if (input.Contains("check"))
+                {
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        if (Char.IsDigit(input[i]))
+                        {
+                            testCol = input[i].ToString();
+                        }
+
+                        if (testCol.Length > 0)
+                        {
+                            val = int.Parse(testCol);
+                        }
+                    }
+
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        int testNumber = Convert.ToInt32(numbers[i]);
+
+                        mask = 1 << val;
+                        //if (testNumber & mask)
+                        //{
+                        //    bits++;
+                        //}
+                    }
+                   
+                }
+                
+            } while (input != "end");
+
+
+           
+        }
+    }
+}
