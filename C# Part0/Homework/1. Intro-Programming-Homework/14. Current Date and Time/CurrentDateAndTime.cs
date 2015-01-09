7f1e61ec87cc055e9076ea910b7570b655e760cd
Todor Dakov <tdd@dekom.bg>
2015-01-09 22:39:48 +0200

    Update

--- C# Part0/Homework/1. Intro-Programming-Homework/14. Current Date and Time/CurrentDateAndTime.cs
+++ C# Part0/Homework/1. Intro-Programming-Homework/14. Current Date and Time/CurrentDateAndTime.cs
@@ -6,7 +6,7 @@ class CurrentDateAndTime
     {
         Console.WriteLine(new string('-', 60));
         DateTime now = DateTime.Now;       
-        Console.WriteLine("The curent date and time is: {0}", now);
+        Console.WriteLine("The current date and time is: {0}", now);
         Console.WriteLine(new string('-', 60));
     }
 }
